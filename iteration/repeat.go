3ebeae61a70607ceb3b93f965de65981c48cff85
walkerxin <1464193658@qq.com>
2020-11-12 08:58:40 +0800

    添加示例函数

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
 // Repeat the taked character return five
-func Repeat(character string) (string) {
+func Repeat(character string, frequency int) string {
 	var repeated string
-	for i:=0; i<5; i++ {
+	for i:=0; i<frequency; i++ {
 		repeated += character
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,10 +1,11 @@
 package iteration
 
+import "fmt"
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeated != expected {
 		t.Errorf("expected '%s' but got '%s'", expected, repeated)
@@ -13,8 +14,14 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i:=0; i<b.N; i++ {
-		Repeat("a")
+		Repeat("a", b.N)
 	}
+}
+
+func ExampleRepeat() {
+	result := Repeat("b", 8)
+	fmt.Println(result)
+	// Output: bbbbbbbb
 }
 
 
@@ -34,5 +41,7 @@ func BenchmarkRepeat(b *testing.B) {
 	- 基准测试运行时，代码会运行 b.N 次，并测量需要多长时间。
 	- 代码运行的次数不会对你产生影响，测试框架会选择一个它所认为的最佳值，以便让你获得更合理的结果。
 	- 用 go test -bench=. 来运行基准测试 （Powershell中 go test -bench="."）
+	- 【输出说明】运行一次函数用了多久，运行次数
 
+4. [strings包](https://golang.org/pkg/strings/)
 */
