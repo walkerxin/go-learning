603b0a2396414fe48035016bd1f2343a331a5034
walkerxin <1464193658@qq.com>
2020-11-12 17:04:48 +0800

    SumAll 得到每个切片和的切片

--- arrayAndSlice/sum.go
+++ arrayAndSlice/sum.go
@@ -6,3 +6,10 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
--- arrayAndSlice/sum_test.go
+++ arrayAndSlice/sum_test.go
@@ -1,6 +1,7 @@
 package arrayAndSlice
 
 import "testing"
+import "reflect"
 
 //【需求1】创建一个 Sum 函数，它使用 for 来循环获取数组中的元素并返回所有元素的总和。
 func TestSum(t *testing.T) {
@@ -16,8 +17,21 @@ func TestSum(t *testing.T) {
 	})
 }
 
+//【需求2】SumAll 函数，它接受多个切片，并返回由每个切片元素的总和组成的新切片。
+func TestSumAll(t *testing.T) {
+	numbers1 := []int{1, 2, 3}
+	numbers2 := []int{4, 5}
 
-/*
+	got := SumAll(numbers1, numbers2)
+	want := []int{6, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+
+}
+
+/*数组：`有序`、`多个相同类型的元素`
 1. 如何声明并初始化一个数组
 	[length]type{value1, value2, ..., valueN}
 	[...]type{value1, value2, ..., valueN}
@@ -40,6 +54,21 @@ FAIL    github.com/amanda/arrayAndSlice [build failed]
 		mySlice := []int{1, 2, 3}
 		mySlice := [3]int{1, 2, 3}
 	- 执行`覆盖率测试工具` go test -cover
+*/
 
+/*【需求2】SumAll
+6. 使用`可变参数` ...[例子](https://gobyexample.com/variadic-functions)
+7. 切片比较
+	invalid operation: got != want (slice can only be compared to nil)
+	在 Go 中不能对切片使用等号运算符。
+	- 你可以写一个函数迭代每个元素来检查它们的值。
+	-【比较简单】是使用 reflect.DeepEqual，它在判断两个变量是否相等时十分有用。
+	-【注意 reflect.DeepEqual 不是「类型安全」的】即使比较的是不同类型，也能通过编译
 
+8. make([]int, length) 
+	- 创建指定类型和长度(容量)的切片
+
+9. 使用 `append` 函数，来为切片追加新值
+	- 数组有越界问题，切片相当于长度不是类型的数组，有容量的概念，所以也存在越界
+	可以通过sums = append(sums, xxx)来实现追加
 */
